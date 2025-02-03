d86a01b73db8c161b46bd3d8ed16863a742c0386
jrommos936 <jrommos936@g.educaand.es>
2025-02-03 12:38:16 +0100

    Validador de contraseñas realizado

--- ValidadorContrasena.js
+++ ValidadorContrasena.js
@@ -1,17 +1,10 @@
-function validatePassword(password) {
-    const minLength = /.{8,}/;
-    const hasUpperCase = /[A-Z]/;
-    const hasLowerCase = /[a-z]/;
-    const hasNumber = /[0-9]/;
-    const hasSpecialChar = /[!@#$%^&*()_+{}|:<>?]/;
-    
-    return (
-        minLength.test(password) &&
-        hasUpperCase.test(password) &&
-        hasLowerCase.test(password) &&
-        hasNumber.test(password) &&
-        hasSpecialChar.test(password)
-    );
-}
-
+// passwordValidator.js 
+function validatePassword(password) { 
+    const minLength = /.{8,}/; 
+    const hasUpperCase = /[A-Z]/; 
+    const hasLowerCase = /[a-z]/; 
+    const hasNumber = /[0-9]/; 
+    const hasSpecialChar = /[!@#$%^&*()_+{}|:<>?]/; 
+    return (minLength.test(password) && hasUpperCase.test(password) && hasLowerCase.test(password) && hasNumber.test(password) && hasSpecialChar.test(password)); 
+} 
 module.exports = validatePassword;
